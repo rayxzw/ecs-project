3b8cb14f531eb70734899978f3bd353b26eaa218
rayxzw <165937899+rayxzw@users.noreply.github.com>
2024-08-17 11:40:26 -0700

    Add files via upload

---
+++ project/index.js
@@ -0,0 +1,27 @@
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = () => {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("bar").style.top = "0";
+  } else {
+    document.getElementById("bar").style.top = "-170px";
+  }
+  prevScrollpos = currentScrollPos;
+}
